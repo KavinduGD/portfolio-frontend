98a05dbf9a35048375d4d1f20bc0d59f9cdce4af
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2026-01-19 17:28:55 +0530

    project page refactor

--- src/pages/projects/components/allProjectsCardsPage/index.tsx
+++ src/pages/projects/components/allProjectsCardsPage/index.tsx
@@ -1,40 +1,49 @@
 import ContentDivider from "../../../../components/contentDivider";
 import type { Project } from "../../../../types";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 type ChildProps = {
-  setProjectID: React.Dispatch<React.SetStateAction<string>>;
   projects: Project[];
 };
 
-function AllProjectsCardsPage({ setProjectID, projects }: ChildProps) {
+function AllProjectsCardsPage({ projects }: ChildProps) {
+  const navigate = useNavigate();
   return (
-    <ContentDivider title="Projects">
-      {projects.map((project) => {
-        return (
-          <div
-            className="px-[20px] py-[20px] border-gray-100 odd:border-r-[1px] border-b-[1px]"
-            key={project.projectID}
-          >
+    <div className=" bg-[#FEFEFE]">
+      <ContentDivider title="Projects">
+        {projects.map((project) => {
+          return (
             <div
-              className="group bg-gray-100/10 px-[20px] py-[10px] rounded-lg shadow-2xs border-[1px] border-gray-100 overflow-hidden cursor-pointe"
-              onClick={() => {
-                setProjectID(project.projectID);
-              }}
+              className="xl:px-[20px] xl:py-[20px] px-[5px] py-[5px] border-gray-100 odd:border-r-[1px] border-b-[1px] "
+              key={project.projectID}
             >
-              <div className="overflow-hidden rounded-md">
-                <img
-                  src={project.projectImageUrls[0]}
-                  alt=""
-                  className="transform transition-transform duration-300 ease-in-out group-hover:scale-110"
-                />
-              </div>
+              <div
+                className="group bg-gray-100/10 px-[20px] py-[10px] rounded-lg shadow-2xs border-[1px] border-gray-100 overflow-hidden cursor-pointe"
+                onClick={() => {
+                  navigate(`/projects/${project.projectID}`);
+                }}
+              >
+                <div className="overflow-hidden rounded-md">
+                  <img
+                    src={project.projectImageUrls[0]}
+                    alt=""
+                    className="transform transition-transform duration-300 ease-in-out group-hover:scale-110"
+                  />
+                </div>
 
-              <p className="text-[17px] font-bold">{project.projectName}</p>
+                <div className="flex justify-between mt-[10px]">
+                  <p className="text-[17px] text-gray-700 font-bold group-hover:text-blue-600 ">
+                    {project.projectName}
+                  </p>
+                  <FaArrowUpRightFromSquare className="text-gray-700  group-hover:text-blue-600" />
+                </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </ContentDivider>
+          );
+        })}
+      </ContentDivider>
+    </div>
   );
 }
 
--- src/pages/projects/components/singleProject/index.tsx
+++ src/pages/projects/components/singleProject/index.tsx
@@ -1,94 +1,104 @@
 import ContentDivider from "../../../../components/contentDivider";
 import type { Project } from "../../../../types";
-import TechnologyCard from "../../../resume/components/technologyCard";
+import { useParams } from "react-router-dom";
 
-function SingleProject({ project }: { project: Project }) {
+function SingleProject({ projects }: { projects: Project[] }) {
+  const { projectID } = useParams<{ projectID: string }>();
+
+  const project = projects.find((p) => p.projectID === projectID);
+
+  if (!project) {
+    return <div>Project not found</div>;
+  }
+
   return (
-    <ContentDivider title={project.projectName} style="block">
-      <div className="px-[20px] pt-[20px]">
-        <div className="flex gap-[10px]">
-          {project.projectImageUrls.map((url) => {
-            return (
-              <img
-                src={url}
-                alt="project image"
-                className="w-1/2 object-cover"
-              />
-            );
-          })}
-        </div>
-        {/* links */}
-        <div className="flex gap-x-[30px] gap-y-[10px] mt-[20px] flex-wrap">
-          {project.links.map((link) => {
-            return (
-              <a
-                href={link.link}
-                target="_blank"
-                className="text-[14px] bg-[#0494FA] text-[#fefefe] px-[6px] py-[1px] rounded-[6px] hover:text-blue-700"
-              >
-                {link.linkName}
-              </a>
-            );
-          })}
-        </div>
-
-        {/* created date */}
-        <div className="flex gap-[10px] mt-[20px] items-start">
-          <p className="text-[#999] text-[14px]">Created Date - </p>
-          <p className="text-gray-500 text-[14px]">{project.startedDate}</p>
-        </div>
-
-        {/* description */}
-        <p className="mt-[5px] leading-7 text-[#7a7a7a] text-[17px]">
-          {project.projectDescription}
-        </p>
-
-        {/* technology */}
-        <div className="flex gap-x-[30px] gap-y-[10px] mt-[30px] flex-wrap">
-          {project.technologies.map((technology) => {
-            return (
-              <img
-                src={technology.icon}
-                alt="technology images"
-                className="w-[50px]"
-              />
-            );
-          })}
-        </div>
-
-        {/* architecture */}
-        <div className="grid grid-cols-1  sm:grid-cols-2 mt-[30px] gap-x-[10px] gap-y-[10px]">
-          {project.architectureImageUrls.map((url) => {
-            return (
-              <div>
-                <p className="text-[#444] font-bold">{url.diagramName}</p>
+    <div className="bg-[#FEFEFE]">
+      <ContentDivider title={project.projectName} style="block">
+        <div className="px-[20px] pt-[20px]">
+          <div className="flex gap-[10px]">
+            {project.projectImageUrls.map((url) => {
+              return (
                 <img
-                  src={url.link}
-                  alt="architecture diagram cover"
-                  className="object-cover1 rounded-lg sm:h-[300px]"
+                  src={url}
+                  alt="project image"
+                  className="w-1/2 object-cover"
                 />
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+          {/* links */}
+          <div className="flex gap-x-[30px] gap-y-[10px] mt-[20px] flex-wrap">
+            {project.links.map((link) => {
+              return (
+                <a
+                  href={link.link}
+                  target="_blank"
+                  className="text-[14px] bg-[#0494FA] text-[#fefefe] px-[6px] py-[1px] rounded-[6px] hover:text-blue-700"
+                >
+                  {link.linkName}
+                </a>
+              );
+            })}
+          </div>
 
-        {/* architecture description */}
-        <p className="mt-[20px] leading-7 text-[#7a7a7a] text-[17px]">
-          {project.architectureDescription}
-        </p>
+          {/* created date */}
+          <div className="flex gap-[10px] mt-[20px] items-start">
+            <p className="text-[#999] text-[14px]">Created Date - </p>
+            <p className="text-gray-500 text-[14px]">{project.startedDate}</p>
+          </div>
 
-        {/* tags */}
-        <div className="flex gap-x-[30px] mt-[10px] flex-wrap gap-y-[10px]">
-          {project.tags.map((tag) => {
-            return (
-              <p className="text-[#0494FA] border-[1px] border-[#0494FA] px-[4px] text-[13px] bg-[#e7f6ff]">
-                {tag}
-              </p>
-            );
-          })}
+          {/* description */}
+          <p className="mt-[5px] leading-7 text-[#7a7a7a] text-[17px]">
+            {project.projectDescription}
+          </p>
+
+          {/* technology */}
+          <div className="flex gap-x-[30px] gap-y-[10px] mt-[30px] flex-wrap">
+            {project.technologies.map((technology) => {
+              return (
+                <img
+                  src={technology.icon}
+                  alt="technology images"
+                  className="w-[50px]"
+                />
+              );
+            })}
+          </div>
+
+          {/* architecture */}
+          <div className="grid grid-cols-1  sm:grid-cols-2 mt-[30px] gap-x-[10px] gap-y-[10px]">
+            {project.architectureImageUrls.map((url) => {
+              return (
+                <div>
+                  <p className="text-[#444] font-bold">{url.diagramName}</p>
+                  <img
+                    src={url.link}
+                    alt="architecture diagram cover"
+                    className="object-cover1 rounded-lg sm:h-[300px]"
+                  />
+                </div>
+              );
+            })}
+          </div>
+
+          {/* architecture description */}
+          <p className="mt-[20px] leading-7 text-[#7a7a7a] text-[17px]">
+            {project.architectureDescription}
+          </p>
+
+          {/* tags */}
+          <div className="flex gap-x-[30px] mt-[10px] flex-wrap gap-y-[10px]">
+            {project.tags.map((tag) => {
+              return (
+                <p className="text-[#0494FA] border-[1px] border-[#0494FA] px-[4px] text-[13px] bg-[#e7f6ff]">
+                  {tag}
+                </p>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </ContentDivider>
+      </ContentDivider>
+    </div>
   );
 }
 
